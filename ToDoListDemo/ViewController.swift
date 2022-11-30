65c142887dc1aa5d0f3a13454d9cb32ffd1fa8b1
jagveer singh <jagveersingh.55555@gmail.com>
2022-11-30 11:44:01 +0530

    Initial Commit

---
+++ ToDoListDemo/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ToDoListDemo
+//
+//  Created by Macbook on 28/11/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var itemArray = ["Milk","Eggs","Water"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark { 
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+   
+    @IBAction func addItoms(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Itom", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default){
+            (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "create new itom"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
